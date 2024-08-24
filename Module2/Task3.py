47b4ba9040adfd6200a6df19e5b57e1c160fcb28
user1 <sandushI@metropolia.fi>
2024-08-24 19:57:39 +0300

    Initial commit

---
+++ Module2/Task3.py
@@ -0,0 +1,11 @@
+#get user input
+length=float(input("Enter the length of the rectangle: "))
+width=float(input("Enter the width of the rectangle: "))
+
+#do the calculations
+area=length*width
+perimeter=2*(length+width)
+
+#print the output
+print("The area of the rectangle is ",area,"cm2")
+print("The perimeter of the rectangle is ",perimeter,"cm2")
