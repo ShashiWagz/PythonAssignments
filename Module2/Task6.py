6a7ac981b5fa0bb77e99822ce0dbc66b0d252486
user1 <sandushI@metropolia.fi>
2024-08-24 21:54:13 +0300

    Initial commit

---
+++ Module2/Task6.py
@@ -0,0 +1,14 @@
+from random import randint, randrange, random
+
+num1 = randint(0, 9)
+num2 = randint(0, 9)
+num3 = randint(0, 9)
+
+print(f'code1 : {num1}{num2}{num3}')
+
+num4 = randint(1, 6)
+num5 = randint(1, 6)
+num6 = randint(1, 6)
+num7 = randint(1, 6)
+
+print(f'code2 :{num4}{num5}{num6}{num7}')
