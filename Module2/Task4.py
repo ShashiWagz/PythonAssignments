26e40b4eac734530f99a6226b5d505089f1b0efd
user1 <sandushI@metropolia.fi>
2024-08-24 20:22:15 +0300

    Initial commit

---
+++ Module2/Task4.py
@@ -0,0 +1,14 @@
+#get user input
+Number1=int(input("Enter your first value: "))
+Number2=int(input("Enter your second value: "))
+Number3=int(input("Enter your third value: "))
+
+#calculate the sum,product and average
+Sum=Number1+Number2+Number3
+Product=Number1*Number2*Number3
+Average=Sum/Product
+
+#print the outputs
+print("Sum is : ," , Sum)
+print("Product is : ", Product)
+print("Average is : ,", Average)
