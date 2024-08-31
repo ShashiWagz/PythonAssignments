552c4e773bb7f6e4b736a1b5547204cd221eb749
user1 <sandushI@metropolia.fi>
2024-08-31 23:26:46 +0300

    Initial commit

--- Module4/Task3.py
+++ Module4/Task3.py
@@ -2,9 +2,20 @@ smallest_number=0
 largest_number=0
 
 while True:
-    user_input =input("Enter a number: ")
+    userInput = input("Enter a number : ")
 
-    if user_input == "" :
+    if userInput == "":
         break
 
+    number = int(userInput)
+
+    if smallest_number == 0 or number < smallest_number:
+        smallest_number = number
+
+    if largest_number ==0 or number > largest_number:
+        largest_number = number
+
+if smallest_number !=0 and largest_number !=0:
+    print(f"The smallest number is: {smallest_number}")
+    print(f"The largest number is: {largest_number}")
 
