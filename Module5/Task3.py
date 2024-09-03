84b863fc9e1e406eb88f1cabbf11c9cd7a21d12d
user1 <sandushI@metropolia.fi>
2024-09-03 13:11:45 +0300

    Initial commit

---
+++ Module5/Task3.py
@@ -0,0 +1,17 @@
+user_input = int(input("Enter a number: "))
+prime_num= True
+if user_input > 1:
+    for i in range(2, int(user_input ** 0.5) + 1):
+        if user_input % i == 0:
+            prime_num = False
+            break
+
+else:
+    prime_num = False
+
+if prime_num:
+    print(user_input, "is a prime number")
+else:
+    print(user_input, "is not a prime number")
+
+
