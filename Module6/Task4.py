2552efd885070b3f25eeadceaddb87ef77e8f0c2
user1 <sandushI@metropolia.fi>
2024-09-06 09:07:45 +0300

    Initial commit

---
+++ Module6/Task4.py
@@ -0,0 +1,11 @@
+def sum_int(numbers):
+    return sum(numbers)
+
+def main():
+    numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+
+    total = sum_int(numbers)
+    print(f"The Total Sum of the list is:{total}")
+
+if __name__ == '__main__':
+    main()
